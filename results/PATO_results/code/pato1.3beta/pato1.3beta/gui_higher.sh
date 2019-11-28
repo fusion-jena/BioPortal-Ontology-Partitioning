9ad6c55196f614723146ee9d57465a9a6cacf225
alsayedal <alsayed.algergawy@uni-jena>
2019-11-28 17:46:22 +0100

    adding results of PATO

---
+++ results/PATO_results/code/pato1.3beta/pato1.3beta/gui_higher.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# $Id: gui.sh 252 2005-04-05 11:40:06Z mrmenken $
+#
+# Copyright (C) 2005 by Maarten Menken
+# MR.Menken@few.vu.nl
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+java -Xmx1600m -cp bin:lib/commons-lang/commons-lang-2.0.jar:lib/jung/colt.jar:lib/jung/commons-collections-3.1.jar:lib/jung/jung-1.5.4.jar:lib/sesame/sesame.jar nl.vu.cs.pato.gui.MainWindow $*
